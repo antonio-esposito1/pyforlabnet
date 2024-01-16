bd6a2323a9f8fda2d7174f072d8bbeea868e2096
antonio-esposito1 <antonio.esposito1@inwind.it>
2024-01-16 11:51:41 +0100

    ho aggiunto le istanze navar101, navar201, navpe225 e navpe226

--- labnet.py
+++ labnet.py
@@ -73,105 +73,7 @@ def netconf_requests_isis_neighbors(device):
    return device.isis_neighbors
 
 if __name__ == '__main__':
-  
-  #python3.9 labnet.py --devicename mivpe015 --username antonio --password admin
-  #parser = ArgumentParser()
-  #parser.add_argument("--devicename", required = "True")
-  #parser.add_argument("--username", required = "True")
-  #parser.add_argument("--password", required = "True")
-  #args = parser.parse_args()
-  
-  #devicename = args.devicename
-  #username = args.username
-  #password = args.password
-   
-  #definisco un istanza della classe Device che è a sua volta definita nel file Device.py
-  #dev = Device(devicename,username,password)
-  
-  # device è il metodo che si connette alla macchina in campo  
-  #device = dev.connectnetconf()
-
-  #D = dict()
-  
-  #Questo ciclo for è il cuore del programma, mi crea un dizionario D con tutti i dati estratti dal device.
-  
-  stringaxml = """
-<isis xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-clns-isis-oper">
-            <instances>
-              <instance>
-                <instance-name>CORE</instance-name>
-                <neighbors>
-                  <neighbor>
-                    <system-id/>
-                    <interface-name/>
-                    <neighbor-state/>
-                    <neighbor-circuit-type/>
-                    <neighbor-media-type/>
-                  </neighbor>
-                </neighbors>
-                <checkpoint-adjacencies>
-              <checkpoint-adjacency>
-                <system-id/>
-              </checkpoint-adjacency>
-            </checkpoint-adjacencies>
-              </instance>
-            </instances>
-          </isis>
-          """
- 
-  #D['device'] = netconfrequest(stringaxml, device)
-  
-  
-
-  #print(D['device'])
-  """
-  mivpe015 = Device('mivpe015', 'antonio', 'admin')
-  nc_get_reply = netconfrequest(stringaxml, mivpe015.connectnetconf())
-  #mivpe015.data = netconfrequest(stringaxml, mivpe015.connectnetconf())
-  #mivpe015.isisinstancename = nc_get_reply.data.find('.//{http://cisco.com/ns/yang/Cisco-IOS-XR-clns-isis-oper}instance-name')
-  #print(mivpe015.isisinstancename.text)
-  isis_neighbors = []
-  xmlns = "http://cisco.com/ns/yang/Cisco-IOS-XR-clns-isis-oper"
-  neighbors = nc_get_reply.data.findall(f'.//{{{xmlns}}}neighbor')
-  for neighbor in neighbors:
-     temp = {}
-     temp['system-id'] = neighbor.find(f'{{{xmlns}}}system-id').text
-     temp['interface-name'] = neighbor.find(f'{{{xmlns}}}interface-name').text
-     temp['neighbor-state'] = neighbor.find(f'{{{xmlns}}}neighbor-state').text
-     temp['neighbor-circuit-type'] = neighbor.find(f'{{{xmlns}}}neighbor-circuit-type').text
-     isis_neighbors.append(temp)
-  mivpe015.isis_neighbors = isis_neighbors
-  print(mivpe015.isis_neighbors)
-
-  
-  #print(netconfrequest(stringaxml, mivpe015.connectnetconf()))
-  #print(mivpe015)
-
-  mivpe016 = Device('mivpe016', 'antonio', 'admin')
-  nc_get_reply = netconf_requests_isis_neighbors(mivpe016.connectnetconf())
-  xmlns = "http://cisco.com/ns/yang/Cisco-IOS-XR-clns-isis-oper"
-  neighbors = nc_get_reply.data.findall(f'.//{{{xmlns}}}neighbor')
-
-
-  isis_neighbors = []
-  xmlns = "http://cisco.com/ns/yang/Cisco-IOS-XR-clns-isis-oper"
-  neighbors = nc_get_reply.data.findall(f'.//{{{xmlns}}}neighbor')
-  for neighbor in neighbors:
-     temp = {}
-     temp['system-id'] = neighbor.find(f'{{{xmlns}}}system-id').text
-     temp['interface-name'] = neighbor.find(f'{{{xmlns}}}interface-name').text
-     temp['neighbor-state'] = neighbor.find(f'{{{xmlns}}}neighbor-state').text
-     temp['neighbor-circuit-type'] = neighbor.find(f'{{{xmlns}}}neighbor-circuit-type').text
-     isis_neighbors.append(temp)
-  mivpe016.isis_neighbors = isis_neighbors
-
-  print(mivpe016.isis_neighbors)
-  #mivpe016.data = netconfrequest(stringaxml, mivpe015.connectnetconf())
-  #print(netconfrequest(stringaxml, mivpe016.connectnetconf()))
-  #print(mivpe016)
-
-  """
-
+    
   mivpe015 = Device('mivpe015', 'antonio', 'admin')
   netconf_requests_isis_neighbors(mivpe015)
   mivpe016 = Device('mivpe016', 'antonio', 'admin')
@@ -180,9 +82,17 @@ if __name__ == '__main__':
   netconf_requests_isis_neighbors(mivar102)
   mivar202 = Device('mivar202', 'antonio', 'admin')
   netconf_requests_isis_neighbors(mivar202)
+  navpe225 = Device('navpe225', 'antonio', 'admin')
+  netconf_requests_isis_neighbors(navpe225)
+  navpe226 = Device('navpe226', 'antonio', 'admin')
+  netconf_requests_isis_neighbors(navpe226)
+  navar101 = Device('navar101', 'antonio', 'admin')
+  netconf_requests_isis_neighbors(navar101)
+  navar201 = Device('navar201', 'antonio', 'admin')
+  netconf_requests_isis_neighbors(navar201)
 
   db = shelve.open('devicedb')
-  for obj in (mivpe015, mivpe016, mivar102, mivar202):
+  for obj in (mivpe015, mivpe016, mivar102, mivar202, navar101, navar201, navpe225, navpe226):
       db[obj.devicename] = obj
   db.close()
 
